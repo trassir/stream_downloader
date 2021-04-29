b75ef85bfb75d0c0cfc17adaffe87c02dbc97f02
JegernOUTT <sergey.vakhreev@gmail.com>
2021-04-30 01:55:34 +0300

    multithreading for youtube downloader

--- stream_downloader/youtube.py
+++ stream_downloader/youtube.py
@@ -1,10 +1,11 @@
-import urllib.request
-import subprocess
-from argparse import ArgumentParser
-from pathlib import Path
 import re
+import subprocess
+import urllib.request
+from argparse import ArgumentParser
+from multiprocessing import Pool, cpu_count
+from pathlib import Path
 from time import sleep
-from typing import List
+from typing import List, Optional, Tuple
 
 from tqdm import tqdm
 
@@ -24,14 +25,14 @@ URL_RE = re.compile(r'^(.+?&sq=)(\d+)&')
 def download_stream(video_url: str,
                     save_filepath: Path,
                     video_len_hours: float = .25,
-                    re_encode: bool = False):
+                    re_encode: bool = False,
+                    download_threads: int = 8):
     tmp_dir = prepare_tmp_file_tree(tmp_parent=save_filepath.parent,
                                     tmp_dir_basename=TMP_DIR_NAME)
     try:
         video_url = _get_video_file_url(video_url)
-        videos = _download(video_url, video_len_hours, tmp_dir)
+        videos = _download(video_url, video_len_hours, tmp_dir, pool_size=download_threads)
         videos = _re_encode(videos, tmp_dir) if re_encode else videos
-        print('Filtering invalid videos...')
         videos = _filter_valid_video(videos)
         print('Concatenating videos...')
         concat_videos(videos, save_filepath, tmp_dir)
@@ -56,10 +57,10 @@ def _get_video_file_url(url: str):
 
     def is_target(request):
         return (
-            'videoplayback' in request.path
-            and request.response is not None
-            and request.response.headers['Content-Type'] in target_content_types
-            and URL_RE.match(request.url) is not None
+                'videoplayback' in request.path
+                and request.response is not None
+                and request.response.headers['Content-Type'] in target_content_types
+                and URL_RE.match(request.url) is not None
         )
 
     target = None
@@ -73,39 +74,43 @@ def _get_video_file_url(url: str):
             begin, end = end, len(driver.requests)
             continue
         break
-    driver.close()
+    driver.quit()
     return target.url
+
+
+def _process_download(in_out: Tuple[str, Path],
+                      retrieve_count: int = 20) -> Optional[Path]:
+    retrieve_count = max(1, retrieve_count)
+    url, vid_out = in_out
+    for retrieve_idx in range(retrieve_count):
+        try:
+            _, msg = urllib.request.urlretrieve(url, vid_out)
+            return vid_out
+        except Exception as e:
+            print(f'Unable to download part {vid_out.stem}: {e}. Trying for {retrieve_idx} time')
+            sleep(1 + retrieve_idx)
+    print(f'Skipped part {vid_out.stem}, unable to download')
+    return None
 
 
 def _download(video_url: str,
               video_len_hours: float,
               tmp_dir: Path,
-              retrieve_count: int = 10) -> List[Path]:
-    retrieve_count = max(1, retrieve_count)
+              pool_size: int = 16) -> List[Path]:
     url, current_part = _parse_video_url(video_url)
     n_parts = int(video_len_hours * 3600 / SEC_PER_PART)
     begin, end = max(1, current_part - n_parts + 1 * bool(n_parts)), current_part
 
-    result = []
     vid_dir = tmp_dir / 'videos'
     vid_dir.mkdir(parents=True)
 
-    for part in tqdm(range(begin, end + 1),
-                     desc='Downloading video parts of the stream: '):
-        vid_out = vid_dir / f'{part}.mp4'
-        loaded = False
-        for retrieve_idx in range(retrieve_count):
-            try:
-                _, msg = urllib.request.urlretrieve(f'{url}{part}', vid_out)
-                loaded = True
-                result.append(vid_out)
-                break
-            except Exception as e:
-                print(f'Unable to download part {part}: {e}. Trying for {retrieve_idx} time')
-                sleep(1 + retrieve_idx)
+    input_output = [(f'{url}{part}', vid_dir / f'{part}.mp4')
+                    for part in range(begin, end + 1)]
 
-        if not loaded:
-            print(f'Skipped part {part}, unable to download')
+    with Pool(pool_size) as p:
+        result = tqdm(p.imap(_process_download, input_output),
+                      desc='Downloading video parts of the stream: ', total=len(input_output))
+        result = [r for r in result if r is not None]
 
     return result
 
@@ -117,26 +122,35 @@ def _parse_video_url(url):
     return match.group(1), int(match.group(2))
 
 
+def _process_re_encode(in_out: Tuple[Path, Path],
+                       rm_processed: bool = True) -> Optional[Path]:
+    video, vid_out = in_out
+    process = subprocess.Popen(
+        _make_ffmpeg_re_encode_cmd(video, vid_out),
+        stdout=subprocess.PIPE,
+    )
+    process.communicate()
+    if rm_processed:
+        video.unlink()
+    if process.returncode != 0:
+        print(f'Unable to re-encode {video}, skipping it')
+        return None
+    else:
+        return vid_out
+
+
 def _re_encode(videos: List[Path],
                tmp_dir: Path,
-               rm_processed: bool = True) -> List[Path]:
+               pool_size: int = cpu_count()) -> List[Path]:
     re_enc_dir = tmp_dir / 'fixed'
     re_enc_dir.mkdir(parents=True)
-    result = []
 
-    for video in tqdm(videos, desc='Re-encoding video parts of the stream: '):
-        vid_out = re_enc_dir / video.name
-        process = subprocess.Popen(
-            _make_ffmpeg_re_encode_cmd(video, vid_out),
-            stdout=subprocess.PIPE,
-        )
-        process.communicate()
-        if rm_processed:
-            video.unlink()
-        if process.returncode != 0:
-            print(f'Unable to re-encode {video}, skipping it')
-            continue
-        result.append(vid_out)
+    input_output = [(video, re_enc_dir / video.name) for video in videos]
+
+    with Pool(pool_size) as p:
+        result = tqdm(p.imap(_process_re_encode, input_output),
+                      desc='Re-encoding video parts of the stream: ', total=len(input_output))
+        result = [r for r in result if r is not None]
 
     return result
 
@@ -145,16 +159,24 @@ def _make_ffmpeg_re_encode_cmd(in_: Path, out: Path):
     return f'ffmpeg -hide_banner -loglevel error -i {in_} -c copy {out}'.split()
 
 
-def _filter_valid_video(videos: List[Path]) -> List[Path]:
-    def is_valid(vid: Path):
-        cmd = _make_ffprobe_cmd(vid)
-        process = subprocess.Popen(cmd, stdout=subprocess.PIPE)
-        process.communicate()
-        if process.returncode != 0:
-            print(f'Invalid video {vid}, skipping it')
-            return False
-        return True
-    return list(filter(is_valid, videos))
+def _is_valid(vid: Path) -> bool:
+    cmd = _make_ffprobe_cmd(vid)
+    process = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+    process.communicate()
+    if process.returncode != 0:
+        print(f'Invalid video {vid}, skipping it')
+        return False
+    return True
+
+
+def _filter_valid_video(videos: List[Path],
+                        pool_size: int = cpu_count()) -> List[Path]:
+    with Pool(pool_size) as p:
+        result = tqdm(p.imap(_is_valid, videos),
+                      desc='Filtering invalid videos: ', total=len(videos))
+        result = [vid for valid, vid in zip(result, videos) if valid]
+
+    return result
 
 
 def _make_ffprobe_cmd(vid: Path):
@@ -168,10 +190,11 @@ def main():
     arg_parser.add_argument('--download_last_hours', type=float, default=.25,
                             help='downloads stream for last given hours')
     arg_parser.add_argument('--re_encode', type=int, default=1)
+    arg_parser.add_argument('--download-threads', type=int, default=4)
 
     args = arg_parser.parse_args()
     download_stream(args.video_url, args.save,
-                    args.download_last_hours, args.re_encode)
+                    args.download_last_hours, args.re_encode, args.download_threads)
 
 
 if __name__ == '__main__':
